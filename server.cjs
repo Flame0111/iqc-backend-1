06866a302f701c990faa8503789c2dabfffb1d68
Flame0111 <rangsimxnt.01@gmail.com>
2026-05-21 08:59:54 +0700

    Refactor server.cjs for improved API structure

--- server.cjs
+++ server.cjs
@@ -11,7 +11,7 @@ app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentia
 app.use(express.json()); 
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_KZ5XLtSWbO0i@ep-holy-star-ao9ueqj9-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  connectionString: "postgresql://neondb_owner:npg_KZ5XLtSWb0Oi@ep-holy-star-ao9ueqj9-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 });
 
 async function initDatabase() {
